875601a84343cb5bf09b258743948346046d040a
JessieGue09 <jessy.v.hdez0028@gmail.com>
2020-03-30 14:04:53 -0700

    Cambios realizados de la ultima clase en la carpeta controller y games.

--- routes/games.js
+++ routes/games.js
@@ -1,91 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-const { Game } = require('./../models/game');
+const {Game} = require('./../models/game');
+const gamesController = require('./../controllers/gamesController');
 
 /* GET games listing. */
-router.get('/', function (req, res, next) {
-
-    // let games = [
-    //     { name: 'Super Mario Bross' },
-    //     { name: 'The leyend of Zelda' },
-    //     { name: 'Sonic' }
-    // ];
-
-    Game.findAll()
-        .then((games) => {
-
-            res.render('games/index', {
-                games: games
-            });
-        })
-        .catch((err) =>{
-            console.error('Error trying to query games: ', err);
-            res.render('games/index', {
-                games: []
-            });
-        });
-
-    // console.log(games);
-
-});
-
+router.get('/', gamesController.read);
 
 //games/delete/?
-router.post('/delete/:id', (req, res, next) =>{
-    let id = req.params.id;
-
-    Game.destroy({
-        where: {
-            id: id
-        }
-    }).then(() =>{
-            res.redirect('/games/')
-    
-        }).catch((err) =>{
-            console.error('Error tryung to delete Game', err);
-            res.redirect('/games/');
-        });
-    //TODO: Handle promise
-});
-
+router.post('/delete/:id', gamesController.delete);
 
 //GET games/create
-router.get('/create', (req, res, next) => {
-    res.render('games/form');
-})
+router.get('/create', gamesController.create);
 
-router.post('/create', (req, res, next) => {
-    console.log(req.body);
-    
-    let name = req.body.name;
+router.post('/create', gamesController.createPost);
 
-    
-    if (name === undefined || name === null || name === ''){
-        res.render('/games/form', {errorMessage: 'Please type a valid name.'});
-    }
-    
-    let game = {
-        //name : name 
-        name,
-    };
+router.get('/update/:id', gamesController.update);
 
-    //Crear nuevo game y mandar a la base de datos
-    Game .create(game)
-        //Caso de exito
-        .then(() => {
-            res.redirect('/games');
-        })
-        //Caso de error
-        .catch((err) => {
-            //Imprimir que ocurrio un error
-            console.error('Error trying to create Game', err);
-            //Volver a enviar el formulario como HTML
-            res.render('/games/form');
-        });
- 
-
-    //res.send('Hola');
-})
+router.post('/update', gamesController.updatePost);
 
 module.exports = router;
